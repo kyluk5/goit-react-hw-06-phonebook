4025ec15d094459b19bbb86aa1359b177a31c81e
Andrii Kulyk <kyluk5@gmail.com>
2020-08-03 19:34:14 +0300

    Updates

---
+++ precache-manifest.b363eb8b56cf9ed75760acba75260437.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550c2c70c3cafb864f913c1c1c926f8a",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "71e7c4847413193b2a6b",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.0fda2c40.chunk.css"
+  },
+  {
+    "revision": "a66a5d90e1075ae5cef5",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.a00ed0b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.a00ed0b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e7c4847413193b2a6b",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.4b455f35.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
