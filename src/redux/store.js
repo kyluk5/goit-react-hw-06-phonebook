2c34773c29692421a6293c0de45e18705ed50268
Andrii Kulyk <kyluk5@gmail.com>
2020-08-04 12:57:23 +0300

    added toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,18 @@
-import { createStore, combineReducers } from "redux";
-// import { configureStore } from "@reduxjs/toolkit";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { createStore, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import contactsReducer from "./reducers/contactsReducer";
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-});
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+// });
 
-const store = createStore(rootReducer, composeWithDevTools());
+// const store = createStore(rootReducer, composeWithDevTools());
+
+const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+  },
+});
 
 export default store;
